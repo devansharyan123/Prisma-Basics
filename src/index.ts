84d8cd7ff354068f2f5164c2f1633bd9533bcf5f
DEVANSH ARYAN <1ds22cb022@dsce.edu.in>
2025-09-10 01:30:10 +0530

    function added

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import { PrismaClient } from "./generated/prisma/index.js"
 
 const client = new PrismaClient()
 
-client.user.create({
+
+async function check()
+{
+  await client.user.create({
     data: {
         username : "Devansh Aryan",
         password : "12345",
         age : 22
     }
 })
+}
+
+check();
