4233e4c38c13bcbc569fdb88158e422ffa2cfa5e
DEVANSH ARYAN <1ds22cb022@dsce.edu.in>
2025-09-10 16:32:49 +0530

    expressified it

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,49 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../prisma/generated/prisma/index.js'
+import express from 'express'
+const app = express()
+app.use(express.json())
+
 
 const prisma = new PrismaClient()
 
-async function getUserWithTodos() {
-  try {
-    const user = await prisma.user.findFirst({
-      where: {
-        id: 1
-      },
-      include: {
-        todos: true // Now this should work
-      }
+app.get('/' , async (req, res)=>{
+  const user = await prisma.user.findMany();
+      res.json({
+       user
     })
-    console.log(user)
-    return user
-  } catch (error) {
-    console.error('Error fetching user:', error)
-    throw error
-  }
-}
+})
+
+app.get('/todos/:id' , async (req,res)=>{
+  const id1 = req.params.id;
+  const user = await prisma.user.findFirst({
+    where : {
+      id : parseInt(id1)
+    },
+    select : {
+        todos : true,
+        username : true ,
+        age : true
+    }
+  })
+  res.json({
+    user
+  })
+})
+
+// async function getUserWithTodos() {
+  
+//     const user = await prisma.user.findFirst({
+//       where: {
+//         id: 1
+//       },
+//       include: {
+//         todos: true // Now this should work
+//       }
+//     })
+//     console.log(user)
+ 
+// }
+
+// getUserWithTodos();
+
+app.listen(3000);
